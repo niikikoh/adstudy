5a2a63cd3e931c577c2a17abfb95ae966c8b7a0c
niikikoh <niikikoh@gmail.com>
2021-05-04 23:51:49 +0900

    no message

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -13,7 +13,6 @@ class PaymentsController < ApplicationController
             currency: "jpy",
         })
 
-        # 売れた時の処理
         if @article.update(sold_flg: true)
             redirect_to article_path(params[:id]), notice: "購入しました"
         end
@@ -21,5 +20,35 @@ class PaymentsController < ApplicationController
             flash[:error] = e.message,
             redirecto_to article_path()
         end
+    end
+
+    rescue Stripe::CardError => e
+        flash[:error] = "#決済(stripe)でエラーが発生しました。{e.message}",
+        render :new,
+    end
+
+    rescue Stripe::InvalidRequestError => e
+        flash.now[:error] = "決済(stripe)でエラーが発生しました（InvalidRequestError）#{e.message}",
+        render :new,
+    end
+
+    rescue Stripe::AuthenticationError => e
+        flash.now[:error] = "決済(stripe)でエラーが発生しました（AuthenticationError）#{e.message}",
+        render :new,
+    end
+
+    rescue Stripe::APIConnectionError => e
+        flash.now[:error] = "決済(stripe)でエラーが発生しました（APIConnectionError）#{e.message}",
+        render :new,
+    end
+
+    rescue Stripe::StripeError => e
+        flash.now[:error] = "決済(stripe)でエラーが発生しました（StripeError）#{e.message}",
+        render :new,
+    end
+
+    rescue => e
+        flash.now[:error] = "エラーが発生しました#{e.message}",
+        render :new,
     end
 end
